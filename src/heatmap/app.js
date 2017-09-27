215e857c77607e4185b0ed59bfe65931b27bf5e9
Colin Sheppard <colinthegaunt@gmail.com>
2017-09-26 20:59:47 -0700

    customize heatmap

---
+++ src/heatmap/app.js
@@ -0,0 +1,72 @@
+/* global window,document */
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import MapGL from 'react-map-gl';
+import DeckGLOverlay from './deckgl-overlay.js';
+
+import {csv as requestCsv} from 'd3-request';
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiY29saW5zaGVwcGFyZCIsImEiOiJjaXBzczVxcHowNGVzaDducnIxOWthNXZ0In0.rDGXDr8YNlpiOmNBondDYA';
+
+// Source data CSV
+const DATA_URL = 'https://raw.githubusercontent.com/LBNL-UCB-STI/beam-viz/master/data/bart.txt'
+
+class Root extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        ...DeckGLOverlay.defaultViewport,
+        width: 500,
+        height: 500
+      },
+      data: null
+    };
+
+    requestCsv(DATA_URL, (error, response) => {
+      if (!error) {
+        const data = response.map(d => ([Number(d.lon), Number(d.lat)]));
+        this.setState({data});
+      }
+    });
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize.bind(this));
+    this._resize();
+  }
+
+  _resize() {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  _onViewportChange(viewport) {
+    this.setState({
+      viewport: {...this.state.viewport, ...viewport}
+    });
+  }
+
+  render() {
+    const {viewport, data} = this.state;
+
+    return (
+      <MapGL
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={this._onViewportChange.bind(this)}
+        mapboxApiAccessToken={MAPBOX_TOKEN}>
+        <DeckGLOverlay
+          viewport={viewport}
+          data={data || []}
+        />
+      </MapGL>
+    );
+  }
+}
+
+render(<Root />, document.body.appendChild(document.createElement('div')));
